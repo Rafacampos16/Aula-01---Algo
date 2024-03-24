4ca65e19236c138bdffb4b08a6cf741a8d9523b7
Rafa <108197293+Rafacampos16@users.noreply.github.com>
2024-03-24 00:15:12 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log('Olá mundo')
+
+alert('Caixa de texto')
+
+//innerHTML
+//innerText
+//TextContent
+
+document.getElementById('msg').innerText = 'Exemplo com <strong> innerText </strong>'
+
+document.getElementById('msg2').textContent = 'Exemplo com <strong> textContent </strong>'
+
+document.getElementById('msg3').innerHTML = 'Exemplo com <strong> innerHTML </strong>'
